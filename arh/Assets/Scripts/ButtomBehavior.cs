0b90f4a889be1b4a07bf47a706852fe75156587a
Tagly5 <leotagliati56@gmail.com>
2023-07-12 09:14:08 -0300

    Troca de cor do botao a cada estado

--- arh/Assets/Scripts/ButtomBehavior.cs
+++ arh/Assets/Scripts/ButtomBehavior.cs
@@ -8,13 +8,14 @@ public class ButtomBehavior : MonoBehaviour
 {
     [SerializeField] private GameObject interactable;
     [SerializeField] private SpriteRenderer _renderer;
-    [SerializeField] private bool isInteracting;
-    private bool isInRange;
+    [SerializeField] private bool isInteracting = false;
+    private bool isInRange = false;
 
 
     public Color inRangeColor;
     public Color outRangeColor;
     public Color interactedColor;
+    private Color previewsColor;
 
     
     // Start is called before the first frame update
@@ -22,31 +23,34 @@ public class ButtomBehavior : MonoBehaviour
     {
         _renderer = GetComponent<SpriteRenderer>();
         outRangeColor = _renderer.color;
+        previewsColor = _renderer.color;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        if (isInRange == true && isInteracting == true)
+        if (isInteracting == true && isInRange == true)
         {
-            
-            
-            
-            
-            
-            
-            
+            _renderer.color = interactedColor;
+            print("Apertei o botao!");
         }
+        else if(isInRange == true && isInteracting == false)
+        {
+            _renderer.color = previewsColor;
+        }
+      
     }
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             _renderer.color = inRangeColor;
+            previewsColor = _renderer.color;
             isInRange = true;
+            print("Posso apertar o botao!");
         }
+        
         
     }
 
@@ -55,6 +59,10 @@ public class ButtomBehavior : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             _renderer.color = outRangeColor;
+            previewsColor = _renderer.color;
+            isInRange = false;
+            print("Nao posso mais apertar o botao!");
+
         }
 
     }
