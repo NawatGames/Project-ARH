e6dfd30ca82328a3218199685823ec8634e1c9df
Tagly5 <leotagliati56@gmail.com>
2023-07-20 10:30:00 -0300

    Botao operavel

--- arh/Assets/Scripts/DoorBehavior.cs
+++ arh/Assets/Scripts/DoorBehavior.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening.Core;
 
 public class DoorBehavior : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
