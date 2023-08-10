dd408646b1b77304fbab2e021e485c6aee333d54
Sakaef03 <fosakae@icloud.com>
2023-08-09 22:24:27 -0300

    correção erros do commit anterior

--- arh/Assets/Scripts/Alien/AlienShrinkingScript.cs
+++ arh/Assets/Scripts/Alien/AlienShrinkingScript.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 
 public class AlienShrinkingScript : MonoBehaviour
 {
-    public bool isShrunk = false;
-
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftControl))//&& isShrunk == false)
+        if(Input.GetKeyDown(KeyCode.LeftControl))
         {
-            //isShrunk = true;
             gameObject.GetComponent<BoxCollider2D>().size = new Vector2(1f, 0.5f);
         }
 
-        if(Input.GetKeyDown(KeyCode.X))// && isShrunk == true)
+        if(Input.GetKeyDown(KeyCode.X))
         {
-            //isShrunk = false;
             gameObject.GetComponent<BoxCollider2D>().size = new Vector2(1f, 1f);
         }
     }
--- arh/Assets/Scripts/Alien/AlienSpriteChanger.cs
+++ arh/Assets/Scripts/Alien/AlienSpriteChanger.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class AlienSpriteChanger : MonoBehaviour
 {
+    private AlienShrinkingScript alienShrinker;
+
+    void Start()
+    {
+        alienShrinker = gameObject.GetComponent<AlienShrinkingScript>();
+    }
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftControl))// && alienShrinker.isShrunk == false)
+        if(Input.GetKeyDown(KeyCode.LeftControl)) //&& alienShrinker.isShrunk == false)
         {
             gameObject.GetComponent<SpriteRenderer>().size = new Vector2 (1f, 0.5f);
         }
 
-        if(Input.GetKeyDown(KeyCode.X))// && alienShrinker.isShrunk == true)
+        if(Input.GetKeyDown(KeyCode.X)) //&& alienShrinker.isShrunk == true)
         {
             gameObject.GetComponent<SpriteRenderer>().size = new Vector2 (1f, 1f);
         }
