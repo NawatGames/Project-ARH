d6da73d2eb327a6a4e348c83f00df47c604ee576
Sakaef03 <fosakae@icloud.com>
2023-08-28 17:34:48 -0300

    criação do teste da mecânica de coyote

--- arh/Assets/Scripts/Player/CoyoteScript.cs
+++ arh/Assets/Scripts/Player/CoyoteScript.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class JetpackPlayerMovement : MonoBehaviour
+public class CoyoteScript : MonoBehaviour
 {
     public float moveSpeed = 8f;
     public float jumpForce = 8f;
@@ -14,6 +13,10 @@ public class JetpackPlayerMovement : MonoBehaviour
     private bool doublejumpEnabled;
     public bool isGrounded;
     public bool jetpackEnabled;
+    private float coyoteTime = 0.2f;
+    public float coyoteCounter;
+    private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
 
     private void Awake()
     {
@@ -29,12 +32,13 @@ public class JetpackPlayerMovement : MonoBehaviour
 
         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if(jumpBufferCounter > 0f)
         {
-            if(isGrounded)
+            if(coyoteCounter > 0f)
             {
                 rb.velocity = new Vector2(moveDirection * moveSpeed, jumpForce);
-                isGrounded = false;
+                //isGrounded = false;
+                jumpBufferCounter = 0f;
             }
 
             else if(doublejumpEnabled)
@@ -42,6 +46,36 @@ public class JetpackPlayerMovement : MonoBehaviour
                 rb.velocity = new Vector2(moveDirection * moveSpeed, jumpForce);
                 doublejumpEnabled = false;
             }
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            if(rb.velocity.y > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+                //comentar ou remover o de cima se não tiver mecânica de alterar força de pulo de acordo com input
+                coyoteCounter = 0f;
+            }
+        }
+
+        if(isGrounded)
+        {
+            coyoteCounter = coyoteTime;
+        }
+
+        else
+        {
+            coyoteCounter -= Time.deltaTime;
         }
     }
 
@@ -55,6 +89,15 @@ public class JetpackPlayerMovement : MonoBehaviour
         if(jetpackEnabled)
         {
             doublejumpEnabled = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            Debug.Log("Saiu");
         }
     }
 
