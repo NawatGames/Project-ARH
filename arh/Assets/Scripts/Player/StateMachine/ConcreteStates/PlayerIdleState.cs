32e38cccfc15e1822cc82281f067ba66244bd60c
Tagly5 <leotagliati56@gmail.com>
2023-09-04 23:12:43 -0300

    Player deslizando resolvido

--- arh/Assets/Scripts/Player/StateMachine/ConcreteStates/PlayerIdleState.cs
+++ arh/Assets/Scripts/Player/StateMachine/ConcreteStates/PlayerIdleState.cs
@@ -18,7 +18,9 @@ public class PlayerIdleState : PlayerBaseState
     }
     public override void PhysicsUpdate()
     {
-        
+        //Debug.Log("Physics Update Called!");
+        Ctx.Rigidbody2D.velocity = new Vector2(0f, Ctx.Rigidbody2D.velocity.y);
+
     }
     public override void ExitState()
     {
