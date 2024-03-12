a23dc48c33baf786f560f5e59f4debcc699ea79c
Sakaef03 <fosakae@icloud.com>
2024-03-11 22:49:36 -0300

    criação do script para a plataforma subir e descer

--- arh/Assets/Code/Scripts/PressurePlate.cs
+++ arh/Assets/Code/Scripts/PressurePlate.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using DG.Tweening.Core;
+using UnityEngine.Events;
 
-public class PressurePlateScript : MonoBehaviour
+public class PressurePlate : MonoBehaviour
 {
-    void Awake()
+    public GameObject platformObj;
+    public float moveTime;
+    public float moveDistance;
+    private float backDistance;
+
+
+    void Start()
     {
-        Debug.Log("Awake");
+        backDistance = platformObj.transform.position.y;
+        Debug.Log(backDistance);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Placa ativada");
         gameObject.transform.localScale -= new Vector3(0f,0.25f,0f);
+        platformObj.transform.transform.DOLocalMoveY(moveDistance, moveTime);
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
+        //StartCoroutine(MoveDownCountdown());
         gameObject.transform.localScale += new Vector3(0f,0.25f,0f);
+        platformObj.transform.transform.DOLocalMoveY(backDistance, moveTime);
+    }
+
+    IEnumerator MoveDownCountdown()
+    {
+        yield return new WaitForSeconds(2.0f);
     }
 }
